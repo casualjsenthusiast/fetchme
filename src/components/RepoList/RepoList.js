cd8b1da9d4138f45f88a03902c528348f947c076
casualjsenthusiast <dharmendranegi998@gmail.com>
2023-02-19 08:10:18 +0530

    Add action buttons for pagination purposes

--- src/components/RepoList/RepoList.js
+++ src/components/RepoList/RepoList.js
@@ -6,14 +6,26 @@ import List from "./List";
 
 const RepoList = (props) => {
   let [repos, setRepos] = useState([]);
+  let [page, setPage] = useState(1);
   let isSidebarClosed = useSelector((state) => state.sidebar.isSidebarClosed);
   let searchTerm = useSelector((state) => state.selectedTerm.selectedTerm);
+  const PER_PAGE = 10;
+
+  const actionButtonClickHandler = (action, _) => {
+    if (action === "next") {
+      setPage((prevPage) => prevPage + 1);
+    } else {
+      if (page > 1) {
+        setPage((prevPage) => prevPage - 1);
+      }
+    }
+  };
 
   useEffect(() => {
     async function fetchData() {
       if (searchTerm.trim() !== "") {
         let data = await fetch(
-          `https://api.github.com/search/repositories?q=${searchTerm}+in:name&sort=stars&order=desc`
+          `https://api.github.com/search/repositories?q=${searchTerm}+in:name&page=${page}&per_page=${PER_PAGE}&sort=stars&order=desc`
         ).then((d) => d.json());
 
         setRepos(
@@ -31,7 +43,7 @@ const RepoList = (props) => {
       }
     }
     fetchData();
-  }, [searchTerm]);
+  }, [searchTerm, page]);
 
   return (
     <section
@@ -41,11 +53,21 @@ const RepoList = (props) => {
     >
       {searchTerm === "" && <NotFound data="No data found" />}
       {searchTerm !== "" && (
-        <div>
-          {repos.map((repo) => (
-            <List key={repo.id} repo={repo} />
-          ))}
-        </div>
+        <>
+          <div>
+            {repos.map((repo) => (
+              <List key={repo.id} repo={repo} />
+            ))}
+          </div>
+          <div className={classes["action-buttons"]}>
+            <button disabled={page === 1} onClick={actionButtonClickHandler.bind(null, "previous")}>
+              Previous
+            </button>
+            <button onClick={actionButtonClickHandler.bind(null, "next")}>
+              Next
+            </button>
+          </div>
+        </>
       )}
     </section>
   );
