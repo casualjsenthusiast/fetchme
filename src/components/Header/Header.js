b7e41191a69255d4ff17aa2314e3db67648e9763
casualjsenthusiast <dharmendranegi998@gmail.com>
2023-02-21 20:30:41 +0530

    Add back button to header on Readme component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,30 @@
 import classes from "./Header.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { pageActions } from "../../store";
 
 const Header = () => {
+  const component = useSelector((state) => state.page.page.component);
+  const dispatch = useDispatch();
+  const onBackClickHandler = () => {
+    dispatch(
+      pageActions.setPage({
+        component: "repoList",
+        queryParams: [],
+      })
+    );
+  };
   return (
     <header className={classes.header}>
-      <span>FetchMe</span>
+      {component !== "readme" && <span>FetchMe</span>}
+      {component === "readme" && (
+        <>
+          <span
+            className="fa fa-arrow-circle-o-left"
+            onClick={onBackClickHandler}
+          ></span>
+          <span>FetchMe</span>
+        </>
+      )}
       <span className="fa fa-link"></span>
       <a
         href="https://github.com/casualjsenthusiast/fetchme"
